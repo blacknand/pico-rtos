a2f41e2b6e42f397203ad241ddd5f629f83329c7
Nathan Blackburn <nblackburndeveloper@icloud.com>
2024-09-12 11:00:46 +0100

    add ifndef checks for LEDS

--- src/task_scheduler/task_scheduler.c
+++ src/task_scheduler/task_scheduler.c
@@ -1,20 +1,43 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+/**
+ *  Pico W microcontrollers use a GPIO on the WiFi chip for the LED,
+ *  so when building for Pico W, CYW43_WL_GPIO_LED_PIN will be defined
+ */
+#ifndef CYW43_WL_GPIO_LED_PIN
+#include "pico/cyw43_arch.h"
+#endif
 
 #ifndef LED_DELAY_MS
 #define LED_DELAY_MS 100
 #endif
 
 
-void pico_led_init(void) {
+int pico_led_init(void) {
+#if defined(PICO_DEFAULT_LED_PIN)
     gpio_init(PICO_DEFAULT_LED_PIN);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    return PICO_OK;
+#elif defined(CYWR3_WL_GPIO_LED_PIN)
+    return cyw43_arch_init();
+#endif
+}
+
+
+void pico_set_led(bool led_on) {
+#if defined(PICO_DEFAULT_LED_PIN)
+    gpio_put(PICO_DEFAULT_LED_PIN, led_on);
+#elif defined(CYW43_WL_GPIO_LED_PIN, led_on)
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
+#endif
 }
 
 
 void flash_pico_led(void) {
     // Arrive at time 0
+    int rc = pico_led_init();
+    hard_assert(rc == PICO_OK);
     for (int i = 0; i < 10; i++) {
         pico_set_led(true);
         sleep_ms(LED_DELAY_MS);
@@ -26,26 +49,23 @@ void flash_pico_led(void) {
 
 void output_task1(void) {
     // Arrive at time 1
-    print("Hello from output_task1\n");
+    printf("Hello from output_task1\n");
     sleep_ms(1000);
 }
 
 
 void output_task2(void) {
     // Arrive at time 2
-    print("Hello from output_task2\n");
+    printf("Hello from output_task2\n");
     sleep_ms(1000);
 }
 
 
 void output_task3(void) {
     // Arrive at time 3
-    print("Hello from output_task3\n");
+    printf("Hello from output_task3\n");
     sleep_ms(1000);
 }
-
-
-
 
 
 int main() {
